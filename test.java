1aa35ed221e7aa21374557a2fa5be61fcec67f41
christopher365 <chr365and@gmail.com>
2025-10-25 17:23:16 +0000

    deleted test.java

--- test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("This is a test file.");
-        // Added a new line for testing purposes
-    }
-}
